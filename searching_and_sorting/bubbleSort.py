5bc50916e788a164764621c5a4ae394fb5f74d63
abiryusuf <yusuf_ctg@yahoo.com>
2021-08-03 22:34:59 -0400

    bubble sort

---
+++ searching_and_sorting/bubbleSort.py
@@ -0,0 +1,24 @@
+"""
+Bubble Sort: Rearranges the value by iterating over the list multiple times, causing
+larger values to bubble to the top or end of the list.
+
+Main reasons is find the large value from end of the list
+Bubble sort needs two loops
+"""
+
+arr = [12, 3, 6, 11, 5, 10]
+
+def bubbleSort(arr):
+    length = len(arr)
+
+    for i in range(length):
+        for j in range(0, length - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+bubbleSort(arr)
+
+for i in range(len(arr)):
+    print(arr[i])
+
