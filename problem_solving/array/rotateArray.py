4982b483769410d1c67aad2eac8933ebdf67a316
abiryusuf <yusuf_ctg@yahoo.com>
2021-08-13 18:51:44 -0400

    update

--- problem_solving/array/rotateArray.py
+++ problem_solving/array/rotateArray.py
@@ -1,7 +1,7 @@
 
 arr = [1, 2, 3, 4, 5, 6, 7]
 
-def arrayRotate(arr, target):
+def arrayRotate(arr, target, n):
     n = len(arr)
 
     temp = []
@@ -9,7 +9,11 @@ def arrayRotate(arr, target):
     while i < target:
         temp.append(arr[i])
         i += 1
+    i = 0
     while target < n:
-        arr[i] = arr[d]
-    return temp
-print(arrayRotate(arr, 3))
+        arr[i] = arr[target]
+        i += 1
+        target += 1
+    arr[:] = arr[: i] + temp
+    return arr
+print(arrayRotate(arr, 3, len(arr)))
