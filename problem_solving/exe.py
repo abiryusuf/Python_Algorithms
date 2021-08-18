3454665122e70d2e38cd020fc3c37af1ef345d87
abiryusuf <yusuf_ctg@yahoo.com>
2021-08-18 17:41:23 -0400

    dict

--- problem_solving/exe.py
+++ problem_solving/exe.py
@@ -1,12 +1,35 @@
-def countLetter(str):
-    str = str.replace(" ", "").lower()
-    count = {}
 
+info = 'abir_yusuf_ny_usa'
+# out put: {0: 'abir', 1: 'yusuf', 2: 'ny', 3: 'usa'}
+# def convertDict(str):
+#     y = "_"
+#     x = str.split(y)
+#     res = {}
+#     for i, value in enumerate(x):
+#         if i not in res:
+#             res[i] = value
+#     return res
+#
+# print(convertDict(info))
+
+arr = [1, 2, 3, 4, 5]
+
+def listDict(arr):
+    res = {}
+    for i, value in enumerate(arr):
+        if i not in res:
+            res[i] = value
+    return res
+print(listDict(arr))
+
+name = "I am abir"
+def countText(str):
+    str = str.replace(" ", "")
+    res = {}
     for i in str:
-        if i in count:
-            count[i] += 1
+        if i in res:
+            res[i] += 1
         else:
-            count[i] = 1
-    return count
-print(countLetter("abir yusuf"))
-
+            res[i] = 1
+    return res
+print(countText(name))
