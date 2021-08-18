e022782a65b3cba100703df8f12223bd9817b9fd
abiryusuf <yusuf_ctg@yahoo.com>
2021-08-17 22:39:44 -0400

    update

---
+++ problem_solving/different_problem/pass_reference_value.py
@@ -0,0 +1,15 @@
+def set_list(list):
+    list = [1, 2, 3]
+    # list.append(89)
+    return list
+
+def add(list):
+    list.append(6)
+    return list
+
+myValue = [7]
+
+print(set_list(myValue))
+
+print(add(myValue))
+
